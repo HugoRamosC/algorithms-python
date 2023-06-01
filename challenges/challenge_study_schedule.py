34944ef5c02062eb8e0d000abacdddbf15587b9f
HugoRamosC <hugor.castilho@gmail.com>
2023-06-01 17:40:48 -0300

    feat: add challenge_study_schedule - req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,22 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # [(1, 3), (2, 5), (1, 4)]
+    hours_dict = {}
+
+    if(target_time == None or not isinstance(permanence_period, list)): return None
+    for student in permanence_period:
+        if(not isinstance(student, tuple)): return None
+        if(type(student[0]) != int or type(student[1]) != int): return None
+        hour = student[0]
+        while hour <= student[1]:
+            if(hour not in hours_dict):
+                hours_dict[hour] = 1
+                hour += 1
+            else:
+                hours_dict[hour] += 1
+                hour += 1
+
+
+
+        # for hour in range(student[0], student[1] + 1):
+    return hours_dict.get(target_time, None)
+    # raise NotImplementedError
