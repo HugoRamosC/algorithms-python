22fc007673c80ec8c87c936714f0c84979d5613a
HugoRamosC <hugor.castilho@gmail.com>
2023-06-01 23:21:19 -0300

    feat: challenge_palindromes_recursive - req 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 1:
+        return True
+
+    elif len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+
+    elif low_index <= high_index:
+        low_index += 1
+        high_index -= 1
+        return is_palindrome_recursive(word, low_index, high_index)
+
+    return True
+    # raise NotImplementedError
