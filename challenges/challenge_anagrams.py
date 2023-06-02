d861d5081c618be4594ae07b18399228700fb8ff
HugoRamosC <hugor.castilho@gmail.com>
2023-06-02 20:22:12 -0300

    refactor: change merge and merge_sort parameter name

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -12,32 +12,32 @@ def is_anagram(first_string: str, second_string: str):
     return (first_sorted, second_sorted, first_sorted == second_sorted)
 
 
-def merge_sort(numbers, start=0, end=None):
+def merge_sort(list, start=0, end=None):
     if end is None:
-        end = len(numbers)
+        end = len(list)
     if (end - start) > 1:
         mid = (start + end) // 2
-        merge_sort(numbers, start, mid)
-        merge_sort(numbers, mid, end)
-        merge(numbers, start, mid, end)
+        merge_sort(list, start, mid)
+        merge_sort(list, mid, end)
+        merge(list, start, mid, end)
 
 
-def merge(numbers, start, mid, end):
-    left = numbers[start:mid]
-    right = numbers[mid:end]
+def merge(list, start, mid, end):
+    left = list[start:mid]
+    right = list[mid:end]
 
     left_index, right_index = 0, 0
 
     for general_index in range(start, end):
         if left_index >= len(left):
-            numbers[general_index] = right[right_index]
+            list[general_index] = right[right_index]
             right_index = right_index + 1
         elif right_index >= len(right):
-            numbers[general_index] = left[left_index]
+            list[general_index] = left[left_index]
             left_index = left_index + 1
         elif left[left_index] < right[right_index]:
-            numbers[general_index] = left[left_index]
+            list[general_index] = left[left_index]
             left_index = left_index + 1
         else:
-            numbers[general_index] = right[right_index]
+            list[general_index] = right[right_index]
             right_index = right_index + 1
