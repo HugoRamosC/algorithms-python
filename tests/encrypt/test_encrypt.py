9447b36e97f81f41008e0c2e20558ae9696aea87
HugoRamosC <hugor.castilho@gmail.com>
2023-06-01 22:34:30 -0300

    test: encrypt_message - req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,23 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
+
+invalid_message = 12345
 
 
 def test_encrypt_message():
-    pass
+    "to success with a odd key"
+    assert encrypt_message("message", 3) == "sem_egas"
+
+    "to success with a pair key"
+    assert encrypt_message("message", 4) == "ega_ssem"
+
+    "if a exception raised with invalid parameters"
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(invalid_message, 4)
+
+    "if a exception raised with invalid parameters"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("message", "invalid_key")
+
+    "if return inverted message with a invalid key"
+    assert encrypt_message("message", 10) == 'egassem'
